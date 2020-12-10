ffc0b21f01919babddb73cb717cfaf53698a4857
Oousmane Oddo <oddomaximum@gmail.com>
2020-12-10 20:52:24 +0000

    correct css and js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,36 +3,20 @@
 <head>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 
-{{-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-130660555-2"></script> --}}
-{{-- <script>
-=======
-<!<script async src="https://www.googletagmanager.com/gtag/js?id=UA-130660555-2"></script>
+{{-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-130660555-2"></script>
 <script>
->>>>>>> cd515e9bffe7a0ddff20d7bd750a902f25336fdd
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'UA-130660555-2');
-<<<<<<< HEAD
 </script>
- --}}
-{{-- ======= --}}
+ --}} 
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{csrf_token()}}">
     <title>@yield('title')</title>
     <link rel="shortcut icon" type="image/png" href="/img/logo-djigui.png">
-    @if($cat == 'news')
-        <!-- Compiled and minified CSS -->
-        {{-- <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"> --}}
-        <link rel="stylesheet" href="/css/materialize.css">
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> --}}
-    @else
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    @endif
-
     <link rel="stylesheet" href="/css/app.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">    
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
@@ -58,9 +42,10 @@
 
 <script type="application/javascript" src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
 <script type="application/javascript" src="{{asset('js/jquery-ui.min.js')}}"></script>
-<script type="application/javascript" src="{{asset('js/materialize.min.js')}}"></script>
-<script type="application/javascript" src="{{asset('js/materialize.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+{{-- <script type="application/javascript" src="{{asset('js/materialize.min.js')}}"></script> --}}
+{{-- <script type="application/javascript" src="{{asset('js/materialize.js')}}"></script> --}}
+
 <script src="{{asset('js/app.js')}}?<?php echo filemtime('js/app.js') ?>"></script>
 <script type="text/javascript">
 
